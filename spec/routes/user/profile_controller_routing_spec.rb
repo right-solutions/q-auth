7850611eb1e3e530e545cb99b2318bf26f89a7c8
RaviShankarArya <rshankar@qwinixtech.com>
2014-12-04 16:13:11 +0530

    profile edit routing spec

--- spec/routes/user/profile_controller_routing_spec.rb
+++ spec/routes/user/profile_controller_routing_spec.rb
@@ -7,5 +7,9 @@ describe User::SettingsController, :type => :controller do
       { :get => "user/profile" }.should route_to(:controller => "user/profile", :action => "index")
     end
 
+    it "routes /user/profile to the user/profile controller and edit action" do
+      { :get => "user/edit" }.should route_to(:controller => "user/profile", :action => "edit")
+    end
+
   end
 end
