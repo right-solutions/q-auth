a9293b467a4cd0b1e9172f1173f57f17e7368e80
RaviShankarArya <rshankar@qwinixtech.com>
2014-12-18 10:52:22 +0530

    show routing spec for members controller

--- spec/routes/user/members_controller_routing_spec.rb
+++ spec/routes/user/members_controller_routing_spec.rb
@@ -7,4 +7,12 @@ describe User::MembersController, :type => :controller do
       :action => "index"
     )
   end
+
+  it "show" do
+    expect(:get => "/user/member/:id").to route_to(
+      :controller => "user/members",
+      :action => "show",
+      :id=> ":id"
+    )
+  end
 end
