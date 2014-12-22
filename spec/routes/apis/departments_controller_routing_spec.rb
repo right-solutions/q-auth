a878772ce23862e301f5139f03fd7a0cbe7cd73c
RaviShankarArya <rshankar@qwinixtech.com>
2014-12-22 14:54:24 +0530

    Routing spec for departments controller API

--- spec/routes/apis/departments_controller_routing_spec.rb
+++ spec/routes/apis/departments_controller_routing_spec.rb
@@ -9,5 +9,12 @@ describe Api::V1::DepartmentsController, :type => :controller do
   end
 
 
+  it "show" do
+    expect(:get => "/api/v1/department/:id").to route_to(
+      :controller => "api/v1/departments",
+      :action => "show",
+      :id=> ":id"
+    )
+  end
 
 end
