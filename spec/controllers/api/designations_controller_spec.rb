7d7621d76bcfb22d8e6012931c95a0ec495c1476
RaviShankarArya <rshankar@qwinixtech.com>
2014-12-22 12:20:49 +0530

    rspec for designation controller API

--- spec/controllers/api/designations_controller_spec.rb
+++ spec/controllers/api/designations_controller_spec.rb
@@ -12,15 +12,14 @@ RSpec.describe Api::V1::DesignationsController, :type => :controller do
       [designation_one, designation_two, designation_three]
 
       get :index
-      @data_1 = Designation.order("title asc").page(@current_page).per(@per_page).all
-      expect(assigns[:data]).to match_array(@data_1)
+      @data = Designation.order("title asc").page(@current_page).per(@per_page).all
+      expect(assigns[:data]).to match_array(@data)
     end
   end
   describe "GET #designation" do
     it "should return a designation" do
       get :show, :id => designation.id
       @data = Designation.where("id = ?", designation.id)
-      binding.pry
       expect(assigns[:data]).to eq(@data.first)
     end
   end
