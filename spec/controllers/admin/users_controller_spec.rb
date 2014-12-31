2d05a16aae1573f142e5b4ff16fcfd59064c6525
RaviShankarArya <rshankar@qwinixtech.com>
2014-12-31 12:03:26 +0530

    Refactored user controller spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -5,12 +5,13 @@ describe Admin::UsersController, :type => :controller do
   let(:designation) {FactoryGirl.create(:designation, :title => "senior Developer")}
   let(:department) {FactoryGirl.create(:department, :name => "testing")}
   let(:user) {FactoryGirl.create(:user)}
+  let(:super_admin_user) {FactoryGirl.create(:super_admin_user)}
   let(:admin_user) {FactoryGirl.create(:admin_user)}
   let(:user_1) {FactoryGirl.create(:user)}
   let(:user_2) {FactoryGirl.create(:user)}
 
   before(:each) do
-    session[:id] = admin_user.id
+    session[:id] = super_admin_user.id
   end
 
   describe "POST create" do
@@ -71,31 +72,39 @@ end
 
 
 describe "GET index" do
-  it "assigns all practice as @practise" do
+  it "assigns all users as @user" do
     [designation,user_1, user_2]
     get :index
-    expect(assigns[:users]).to match_array([admin_user,user_1, user_2])
+    expect(assigns[:users]).to match_array([super_admin_user,user_1, user_2])
   end
 end
 
-it "GET make_admin" do
-  get :make_admin, {:user_id => user.id.to_s}
-    expect(assigns(:user)).to eq (user)
+describe "Get admin" do
+  it "GET make_admin" do
+    get :make_admin, {:user_id => user.id}
+    expect(assigns[:user].user_type).to eq("admin")
+  end
 end
 
-it "GET make_super_admin" do
-  get :make_super_admin, {:user_id => user.id.to_s}
-    expect(assigns(:user)).to eq (user)
+describe "Get super admin" do
+  it "GET make_super_admin" do
+    get :make_super_admin, {:user_id => user.id.to_s}
+    expect(assigns[:user].user_type).to eq("super_admin")
+  end
 end
 
-it "GET remove_admin" do
-  get :remove_admin, {:user_id => user.id.to_s}
-    expect(assigns(:user)).to eq (user)
+describe "Get remove admin" do
+  it "GET remove_admin" do
+    get :remove_admin, {:user_id => user.id.to_s}
+    expect(assigns[:user].user_type).to eq("user")
+  end
 end
 
-it "GET remove_super_admin" do
-  get :remove_super_admin, {:user_id => user.id.to_s}
-    expect(assigns(:user)).to eq (user)
+describe "Get remove super admin" do
+  it "GET remove_super_admin" do
+    get :remove_super_admin, {:user_id => user.id.to_s}
+    expect(assigns[:user].user_type).to eq("user")
+  end
 end
 
 end
