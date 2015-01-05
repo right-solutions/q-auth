fb95627017f80aae43b7d81aa26d2f8535913bea
anjankumarhn <aknagaraja@qwinixtech.com>
2015-01-05 16:23:16 +0530

    adding negative rspec cases for admin users update_status method.

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -79,61 +79,94 @@ describe Admin::UsersController, :type => :controller do
     end
 
     describe "PUT update_status" do
-      it "super admin should be able to update user status" do
-        ["active", "suspended","inactive"].each do |status|
-          put :update_status, {:format => :js, :user_id => user1.to_param, :status => status}, {id: super_admin.id}
-          expect(assigns(:user)).to eq (user1)
+      describe "possitive cases" do
+        it "super admin should be able to update user status as active" do
+          put :update_status, {:format => :json, :user_id => user1.to_param, :status => "active"}, {id: super_admin.id}
+          expect(assigns(:user).status).to eq ("active")
         end
+
+        it "super admin should be able to update user status as suspended" do
+         put :update_status, {:format => :json, :user_id => user1.to_param, :status => "suspended"}, {id: super_admin.id}
+         expect(assigns(:user).status).to eq ("suspended")
+       end
+
+       it "super admin should be able to update user status as inactive" do
+         put :update_status, {:format => :json, :user_id => user1.to_param, :status => "inactive"}, {id: super_admin.id}
+         expect(assigns(:user).status).to eq ("inactive")
+       end
+     end
+
+     describe "negative cases" do
+      it "super admin should not be able to update user status as pending" do
+        put :update_status, {:format => :json, :user_id => user1.to_param, :status => "pending"}, {id: super_admin.id}
+        response_body = JSON.parse(response.body)
+
+        expect(response_body["status"]).to eq(["is not included in the list", "pending valid name...", "is invalid"])
+      end
+
+      it "super admin should not be able to update user status as blocked" do
+        put :update_status, {:format => :json, :user_id => user1.to_param, :status => "blocked"}, {id: super_admin.id}
+        response_body = JSON.parse(response.body)
+
+        expect(response_body["status"]).to eq(["is not included in the list", "blocked valid name...", "is invalid"])
+      end
+
+      it "super admin should not be able to update user status as approved" do
+        put :update_status, {:format => :json, :user_id => user1.to_param, :status => "approved"}, {id: super_admin.id}
+        response_body = JSON.parse(response.body)
+
+        expect(response_body["status"]).to eq(["is not included in the list", "approved valid name...", "is invalid"])
       end
     end
-
-    describe "GET edit" do
-      it "assigns the requested user as @user" do
-        get :edit, {:id => user1.to_param}, {id: super_admin.id}
-        expect(assigns(:user)).to eq (user1)
-      end
-    end
-
-    describe "DELETE destroy" do
-      it "super admin should be able to destroy the user" do
-        expect do
-          delete :destroy, {:id => user1.to_param}, {id: super_admin.id}
-          expect(User.count).to  eq 1
-        end
-      end
-    end
-
-    describe "Make admin" do
-      it "super admin should be able to upgrade a user to admin" do
-        get :make_admin, {:user_id => user1.id}, {id: super_admin.id}
-        expect(assigns[:user].user_type).to eq("admin")
-      end
-    end
-
-    describe "Make super admin" do
-      it "super admin should be able to upgdate a user to super admin" do
-        get :make_super_admin, {:user_id => user1.id}, {id: super_admin.id}
-        expect(assigns[:user].user_type).to eq("super_admin")
-      end
-
-      it "super admin should be able to upgdate an admin to super admin" do
-        get :make_super_admin, {:user_id => admin.id}, {id: super_admin.id}
-        expect(assigns[:user].user_type).to eq("super_admin")
-      end
-    end
-
-    describe "Get remove admin" do
-      it "super admin should be able to downgrade an admin to user" do
-        get :remove_admin, {:user_id => admin.id}, {id: super_admin.id}
-        expect(assigns[:user].user_type).to eq("user")
-      end
-    end
-
-    describe "Get remove super admin" do
-      it "super admin should be able to downgrade a super admin to admin" do
-        get :remove_super_admin, {:user_id => super_admin2.id}, {id: super_admin.id}
-        expect(assigns[:user].user_type).to eq("admin")
-      end
-    end
-
   end
+
+  describe "GET edit" do
+    it "assigns the requested user as @user" do
+      get :edit, {:id => user1.to_param}, {id: super_admin.id}
+      expect(assigns(:user)).to eq (user1)
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "super admin should be able to destroy the user" do
+      expect do
+        delete :destroy, {:id => user1.to_param}, {id: super_admin.id}
+        expect(User.count).to  eq 1
+      end
+    end
+  end
+
+  describe "Make admin" do
+    it "super admin should be able to upgrade a user to admin" do
+      get :make_admin, {:user_id => user1.id}, {id: super_admin.id}
+      expect(assigns[:user].user_type).to eq("admin")
+    end
+  end
+
+  describe "Make super admin" do
+    it "super admin should be able to upgdate a user to super admin" do
+      get :make_super_admin, {:user_id => user1.id}, {id: super_admin.id}
+      expect(assigns[:user].user_type).to eq("super_admin")
+    end
+
+    it "super admin should be able to upgdate an admin to super admin" do
+      get :make_super_admin, {:user_id => admin.id}, {id: super_admin.id}
+      expect(assigns[:user].user_type).to eq("super_admin")
+    end
+  end
+
+  describe "Get remove admin" do
+    it "super admin should be able to downgrade an admin to user" do
+      get :remove_admin, {:user_id => admin.id}, {id: super_admin.id}
+      expect(assigns[:user].user_type).to eq("user")
+    end
+  end
+
+  describe "Get remove super admin" do
+    it "super admin should be able to downgrade a super admin to admin" do
+      get :remove_super_admin, {:user_id => super_admin2.id}, {id: super_admin.id}
+      expect(assigns[:user].user_type).to eq("admin")
+    end
+  end
+
+end
