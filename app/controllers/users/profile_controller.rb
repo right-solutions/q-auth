c72f71c5483274bba9918eca4fe9b9b2f27f9f28
anjankumarhn <anjangowdamandya@gmail.com>
2015-04-22 17:13:14 +0530

    Done with reset password functionality.

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -13,15 +13,19 @@ module Users
     def update
       @user = @current_user
       @user.assign_attributes(user_params)
-      @user.save
-      set_flash_message("Profile details has been updated successfully", :success) if @user.errors.blank?
-      render_or_redirect(@user.errors.any?, users_profile_path, "edit")
+      if @user.save
+        set_flash_message("Profile details has been updated successfully", :success) if @user.errors.blank?
+        render_or_redirect(@user.errors.any?, users_profile_path, "edit")
+      else
+        set_flash_message("Failed to update profile", :error)
+        render "edit"
+      end
     end
 
     private
 
     def user_params
-      params[:user].permit(:name, :username, :email, :phone, :designation_overridden, :linkedin, :skype, :department_id, :designation_id)
+      params[:user].permit(:name, :username, :email, :phone, :designation_overridden, :linkedin, :skype, :department_id, :designation_id, :password, :password_confirmation )
     end
 
     def set_navs
@@ -30,4 +34,3 @@ module Users
 
   end
 end
-
